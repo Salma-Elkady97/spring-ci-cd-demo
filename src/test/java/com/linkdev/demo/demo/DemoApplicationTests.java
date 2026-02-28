27e63bbd3b3badb3b5e08fed180ee40c4800065e
Salma Elkady <salma.elkady@linkdev.com>
2026-02-28 03:10:50 +0200

    Initial Spring Boot CI/CD demo

---
+++ src/test/java/com/linkdev/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.linkdev.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void healthReturnsOk() {
+        HealthController c = new HealthController();
+        assertThat(c.health()).isEqualTo("OK");
+    }
+
+}
